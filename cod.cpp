d45dd6939296b2378f7bec6f10805fda01198792
lad98 <ldanielajimenezm@gmail.com>
2025-03-09 18:38:21 -0500

    Create cod.cpp
    Código en c++

---
+++ cod.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+int S;
+int main(){
+std::cout << "Ingrese el total de sus ingresos : ";
+std::cin >>S;
+    // Ciclo para asegurar que la entrada sea un número válido
+while (S<0) {
+            // Se produjo un error, mostramos el mensaje
+        std::cout << "Error: entrada no válida. Intenta nuevamente con un valor positivo.";
+        std::cout << "Ingrese el total de sus ingresos : ";
+        std::cin >>S;
+        
+    }
+ if (S<=10000){
+     std::cout<< "El valor a pagar es : 0 monedas de oro";
+ }
+else {
+    if(10001>= S || S<50000) { 
+        S=S - 10000 ;
+        S=S/10;
+        std::cout<< "El valor a pagar es : "<<S<<" monedas de oro";
+        } 
+    if(S>=50000) {
+        if (S<90000){
+            S=S/10;
+            std::cout<< "El valor a pagar es : "<<S<<" monedas de oro";
+        }else {
+            S=(S*2)/10;
+            std::cout<< "El valor a pagar es : "<<S<<" monedas de oro";
+        }
+    }
+        
+}
+    return 0;
+}
